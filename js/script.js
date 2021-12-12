565ed29835472a13e8a4a0dbf241cd345f72ce00
NicholasOwen <53136434+KatakSawah@users.noreply.github.com>
2021-12-12 20:05:36 +0700

    Kokomited Suicide

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function start_am() {
+    const sect = document.querySelectorAll('.sec_animate');
+
+    observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                document.getElementById("navbar-example").classList.add('bg-transparent');
+                document.getElementById("navbar-example").classList.remove('bg-dark');
+            } else {
+                document.getElementById("navbar-example").classList.add('bg-dark');
+                document.getElementById("navbar-example").classList.remove('bg-transparent');
+            }
+        });
+    });
+
+    sect.forEach(sect_x => {
+        observer.observe(sect_x);
+    });
+}
+
+function changeBgColor() {
+    var url = window.location.href;
+
+    console.log(url);
+
+    if (!url.includes("#product") || !url.includes("#about") || !url.includes("#event") || !url.includes("#contact")) {
+        document.getElementById("navbar-example").classList.add('bg-dark');
+        document.getElementById("navbar-example").classList.remove('bg-transparent');
+    } else {
+        document.getElementById("navbar-example").classList.add('bg-transparent');
+        document.getElementById("navbar-example").classList.remove('bg-dark');
+    }
+}
